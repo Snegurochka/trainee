a3b7cf1fc1a593d3bda0dcf3df71b0c7c1ead8cf
a.burmasova <snegurka@web-esse.ru>
2023-01-06 12:42:24 +0500

    feat: added base components

---
+++ client/src/components/Base.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@mui/material";
+import styled from "@emotion/styled";
+
+export const FlexBox = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  marginTop: "20px",
+});
